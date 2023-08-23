b12f5899f13cf049c5793f780fa44ff99590ba80
Sagar <04017711721_iot@vips.edu>
2023-08-23 12:19:45 +0530

    update feature is to corrected

--- frontend/src/modules/updatecontact/pages/Updatecontact.jsx
+++ frontend/src/modules/updatecontact/pages/Updatecontact.jsx
@@ -8,29 +8,47 @@ import { useState } from 'react';
 
 const Updatecontact =  () => {
 
-    // const [Message , setMessage ] = useState('');
+    const [Message , setMessage ] = useState('');
     // making the reference of the old data of the text field 
     const oldnameRef = useRef();
     const oldemailRef = useRef();
     const olddecsRef = useRef();
     const oldmobileRef = useRef();
-
+    
+    const newnameRef = useRef();
+    const newemailRef = useRef();
+    const newdecsRef = useRef();
+    const newmobileRef = useRef();
 console.log('Hello I am update')
+
+
+const updatecontactId = async()=>{
     const oldcontactInfo = {
         "name" : oldnameRef.current.value,
         "email" : oldemailRef.current.value,
         "desc" : olddecsRef.current.value,
         "mobile" : oldmobileRef.current.value
     }
-        console.log("name  = " , oldcontactInfo.name)
+    
+    const newcontactInfo = {
+        "name" : newnameRef.current.value,
+        "email" : newemailRef.current.value,
+        "desc" : newdecsRef.current.value,
+        "mobile" : newmobileRef.current.value
+    }
 
-
-    const getId = async()=>{
-        try {
+    try {
             const res = await apiclient.get("http://localhost:1234/getcontact" , oldcontactInfo );
             console.log('resssss',res);
             const contactId =  res.data.record._id;
-            // setMessage("Id recevied ");
+            
+        // put the data 
+
+            const res2 = await apiclient.put("http://localhost:1234/updatecontact/"+contactId  , newcontactInfo);
+            
+            console.log("Response 2 is " , res2);
+
+            setMessage("Id recevied ");
             console.log("Contact Id = " , contactId);
         }
         catch(err){
@@ -41,7 +59,7 @@ console.log('Hello I am update')
 return (
     <>
         <container>
-        {/* <p> {Message} </p> */}
+        <p> {Message} </p>
         <Box sx={{ width: 1 }}>
             <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
 
@@ -65,9 +83,29 @@ return (
                 {/* <TextField inputRef = {olddecsRef} id="outlined-basic" label="Old Description" variant="outlined" /> */}
                 </Box>
 
+                <Box gridColumn="span 3">
+                    <input type="text" ref={newnameRef} placeholder='Type name' />
+                {/* <TextField inputRef = {oldnameRef} id="outlined-basic" label="Old Name" variant="outlined" /> */}
+                </Box>
+
+                <Box gridColumn="span 3">
+                <input type="text" ref={newemailRef} placeholder=' old Email' />
+                {/* <TextField inputRef = {oldemailRef} id="outlined-basic" label="Old Email" variant="outlined" /> */}
+                </Box>
+
+                <Box gridColumn="span 3">
+                <input type="text" ref={newmobileRef} placeholder='old mobile' />
+                {/* <TextField inputRef = {oldmobileRef} id="outlined-basic" label="Old Mobile" variant="outlined" /> */}
+                </Box>
+
+                <Box gridColumn="span 3">
+                <input type="text" ref={newdecsRef} placeholder='Old Desc ' />
+                {/* <TextField inputRef = {olddecsRef} id="outlined-basic" label="Old Description" variant="outlined" /> */}
+                </Box>
+
             </Box>
         </Box>
-        <Button onClick={()=>{getId()}}  variant="contained">Update Contact</Button>
+        <Button onClick={updatecontactId}  variant="contained">Update Contact</Button>
         </container>
     </>
 )
