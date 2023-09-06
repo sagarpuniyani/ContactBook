bea5b27436f3590ccb7ad295704a1218a2e63352
sagar <04017711721_iot@vips.edu>
2023-09-06 10:37:47 +0530

    There is the UI created for the FindContact

--- frontend/src/modules/readcontact/components/Findcontact.jsx
+++ frontend/src/modules/readcontact/components/Findcontact.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
 
-const Findcontact = () => {
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+// This component will print the Contact Card 
+
+const Findcontact = (props) => {
 return (
     <>
+    <p>Details are 🙎🏿</p>
+    <Box sx={{ width: 1 }}>
+        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+        <Box gridColumn="span 6">
+            <Item> 😁 {props.Name} </Item>
+        </Box>
+        <Box gridColumn="span 6">
+            <Item> 📳 {props.Mobile}</Item>
+        </Box>
+        <Box gridColumn="span 6">
+            <Item> ✅ {props.Desc}</Item>
+        </Box>
+        <Box gridColumn="span 6">
+            <Item> 📩 {props.Email}</Item>
+        </Box>
+        </Box>
+    </Box>
         
     </>
 )
--- frontend/src/modules/readcontact/pages/Readcontact.jsx
+++ frontend/src/modules/readcontact/pages/Readcontact.jsx
@@ -1,55 +1,60 @@
-import { Container , TextField } from '@mui/material'
-import React from 'react';
+import { Container, TextField } from '@mui/material';
+import React, { useRef, useState} from 'react';
 import Box from '@mui/material/Box';
-import { useRef } from 'react';
 import Button from '@mui/material/Button';
 import { apiclient } from '../../../shared/services/api-client';
+import Findcontact from '../components/Findcontact';
 
-const Readcontact =  () => {
-
+const Readcontact = () => {
+    const [contact, setContact] = useState(null);
     const nameRef = useRef();
     const emailRef = useRef();
 
-    const FindContact = async () => {
+    const FindContactCard = async () => {
         const contactInfo = {
         name: nameRef.current.value,
-        email: emailRef.current.value
-        };
-    
-        try {
-        console.log("From Get Contact : " , contactInfo);
-        const res = await apiclient.post("http://localhost:1234/getcontact", contactInfo);
-        console.log("Res =", res);
-        
-        } catch (err) {
-        console.error("Error:", err);
-
-        }
+        email: emailRef.current.value,
     };
 
+    try {
+        console.log("From Get Contact: ", contactInfo);
+        const res = await apiclient.post("http://localhost:1234/getcontact", contactInfo);
+        setContact(res.data.record);
+        console.log("Res =", res.data.record);
+    } catch (err) {
+        console.error("Error:", err);
+    }
+    };
 
+    
 
 return (
-    <>
-        <Container >
+    <Container>
         <Box sx={{ width: 1 }}>
-            <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
-
-                <Box gridColumn="span 3">
-                <TextField inputRef={nameRef} id="outlined-basic" label="Name" variant="outlined" />
-                </Box>
-
-                <Box gridColumn="span 3">
-                <TextField inputRef={emailRef}  id="outlined-basic" label="Email" variant="outlined" />
-                </Box>
-
+        <Box display="grid" gridTemplateColumns="repeat(12, 1fr)" gap={2}>
+            <Box gridColumn="span 3">
+            <TextField inputRef={nameRef} id="outlined-basic" label="Name" variant="outlined" />
             </Box>
+            <Box gridColumn="span 3">
+            <TextField inputRef={emailRef} id="outlined-basic" label="Email" variant="outlined" />
         </Box>
+        </Box>
+        </Box>
+        <Button onClick={FindContactCard} variant="contained">
+            Find The Contact
+        </Button>
+        {contact ? (
+        <Findcontact
+        Name={contact.name}
+        Desc={contact.desc}
+        Email={contact.email}
+        Mobile={contact.mobile}
+        />
+        ) : (
+        <p>No Contact Found</p>
+        )}
+    </Container>
+);
+};
 
-        <Button onClick={FindContact} variant="contained">Find The Contact</Button>
-        </Container>
-    </>
-)
-}
-
-export default Readcontact
+export default Readcontact;
